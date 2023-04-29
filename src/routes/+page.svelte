4b1a24eeced92f40041ffbc66ef4263b8b9dee77
Justin Hathaway <hathaway.justin@gmail.com>
2023-04-29 00:06:10 -0400

    add pretty background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,10 @@
 <style>
 	:global(body) {
 		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		background: url('https://teamwoodgames.com/_app/immutable/assets/bg-field-sky.40b4b356.png');
+		min-height: 100vh;
+		background-size: cover;
+		overflow: hidden;
 	}
 
 	h1 {
