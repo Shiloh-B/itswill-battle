a26c4a9063f673b47abfdfa3fc9624b28c761abb
Justin Hathaway <hathaway.justin@gmail.com>
2023-05-20 02:54:28 -0400

    Update +page.svelte

--- src/routes/round/+page.svelte
+++ src/routes/round/+page.svelte
@@ -5,10 +5,61 @@
 
   // create a store with a copy of the characters so we can mutate and render the components based on the state of this store
   let roundStateStore = generateRoundStateStore($gameState.teamData.characters, $gameState.oppTeamData?.teamData?.characters); 
-
-
 </script>
 
-{#each $roundStateStore.playerTeam.characters as character}
+<!-- {#each $roundStateStore.playerTeam.characters as character}
   <Character {character}/>
-{/each}
+{/each} -->
+
+<!-- ********************************************************************************************** -->
+
+<header>
+  
+</header>
+
+
+
+
+<div class="container">
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</div>
+
+
+<style>
+  .container {
+    border: 2px solid rgb(25, 185, 4);
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  ul {
+    list-style: none; 
+    display: flex;
+    flex-direction: row;
+    border: 1px solid red;
+    padding: 10px;
+    margin: 20px;
+    justify-content: space-between;
+    width: 40%;
+  }
+
+  li {
+    padding: 8px;
+    width: 80px;
+    height: 80px;
+    background: black;
+  }
+</style>
